7efd0f3d83f72a92e5a7fd52502ef3570ed378ef
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-07 12:04:30 +0900

    [Silver II] Title: 외판원 순회 2, Time: 4292 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -6,24 +6,22 @@ costs = [list(map(int, input().split())) for _ in range(N)]
 
 min_cost = sys.maxsize
 
-def dfs(cost, visited, cur_idx, start_idx):
+def dfs(cost, visited, cur_city, start_city):
     global min_cost
 
     if all(visited):
-        if costs[cur_idx][start_idx] != 0:
-            min_cost = min(min_cost, cost+costs[cur_idx][start_idx]) # 출발지로 돌아가는 금액 추가
+        if costs[cur_city][start_city] != 0:
+            min_cost = min(min_cost, cost + costs[cur_city][start_city])
         return
     
-    # 전체 방문이 아닌 경우
     for i in range(N):
-        if not visited[i] and costs[cur_idx][i] != 0:
+        if not visited[i] and costs[cur_city][i] != 0 :
             visited[i] = True
-            dfs(cost+costs[cur_idx][i], visited, i, start_idx)
+            dfs(cost+costs[cur_city][i], visited, i, start_city)
             visited[i] = False
 
-
 for i in range(N):
-    visited = [False] * N
+    visited = [False]*N
     visited[i] = True
     dfs(0, visited, i, i)
 
