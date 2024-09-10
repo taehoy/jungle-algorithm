7d615f5702dc815f1d1ecf4dcd6daca2b3f49f00
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-10 21:40:00 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,54 +1,47 @@
 import sys
-# sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 N = int(input())
 
-# 준비물 
 board = [list(map(int, input().split())) for _ in range(N)]
-whiteCount = 0
+
+whileCount = 0 
 blueCount = 0
 
-# 체크함수
-def check(startX, startY, endX, endY):
-    startColor = board[startX][startY] # 색종이의 처음 색
+def checkColor(startX, startY, endX, endY):
+
+    startColor = board[startX][startY]
 
     for i in range(startX, endX):
         for j in range(startY, endY):
-            if not startColor == board[i][j] :
+            if not board[i][j] == startColor :
                 return 0
     
-    if startColor == 1 :
-        return 2
+    if startColor == 0 :
+        return 1 # 흰색인 경우 1
     else :
-        return 1
+        return 2 # 파란색인 경우 2
 
-# 나누기 함수
-def divMap(startX, startY, endX, endY):
-    global whiteCount, blueCount
+def sepBoard(startX, startY, endX, endY):
+    global whileCount, blueCount
 
-    # 체크했는데 모두 흰색이면 흰색 색종이 +1하고 모두 파란색이면 파란색색종이 +1한다.
-    status = check(startX, startY, endX, endY)
+    status = checkColor(startX, startY, endX, endY) 
 
-    # 만약 같지 않다면 네등분해준다.
-    if status == 1 : # 1이면 흰색
-        whiteCount += 1
+    if status == 1 :
+        whileCount += 1
         return
-    elif status == 2 : #2면 파란색
+    elif status == 2 :
         blueCount += 1
         return
     else :
-        # 같지 않으니 네등분한다.
-        midX = (endX + startX) // 2 
-        midY = (endY + startY) // 2
+        midX = (startX + endX) // 2
+        midY = (startY + endY) // 2
 
-        divMap(startX, startY, midX, midY) # 0,0, 4, 4
-        divMap(midX, startY, endX, midY) # 4,0,8,4
-        divMap(startX, midY, midX, endY) # 0,4,4,8
-        divMap(midX, midY, endX, endY) # 4,4,8,8
-    
+        sepBoard(startX, startY,midX,midY)
+        sepBoard(midX, startY,endX,midY)
+        sepBoard(startX, midY,midX,endY)
+        sepBoard(midX, midY,endX,endY)
 
-divMap(0,0,N,N)
-
-print(whiteCount)
+sepBoard(0,0,N,N)
+print(whileCount)
 print(blueCount)
