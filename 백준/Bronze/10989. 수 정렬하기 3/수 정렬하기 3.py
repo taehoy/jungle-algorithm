5382975f011651e5260bbe5a6c33c46c166678bc
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-11 16:11:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7908 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+
+arr = [0] * (10000+1)
+
+for _ in range(N) :
+    arr[int(input())] += 1
+
+for i in range(len(arr)):
+    while arr[i] > 0:
+        print(i)
+        arr[i] -= 1
+
