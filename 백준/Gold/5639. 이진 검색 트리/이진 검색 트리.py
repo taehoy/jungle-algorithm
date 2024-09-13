823f3d7a175f1ecfea4d64f640e089118ccb7b8e
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-13 21:29:35 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2320 ms, Memory: 424768 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -5,35 +5,32 @@ sys.setrecursionlimit(10**9)
 arr = []
 
 while True :
-    try:
-        x = int(input())
-        arr.append(x)
-    except ValueError :
+    try :
+        arr.append(int(input()))
+    except :
         break
 
-def post_order_traversal(A):
-    # 길이가 0 이면 리턴
-    if len(A)==0:
+def post_order_to_reverse(A):
+    # 길이가 0인 경우 리턴
+    if len(A) == 0 :
         return
-    
-    # 첫번째 값이 루트값
+    # 왼쪽 오른쪽 리스트 생성
     tempL, tempR = [], []
+    # 루트값 정의
     root = A[0]
 
-    # 인덱스 1부터 len(a)-1까지 순회
+    # 루트값과 비교하여 큰경우 자르기 시전
     for i in range(1, len(A)):
         if A[i] > root :
-            tempR = A[i:]
             tempL = A[1:i]
+            tempR = A[i:]
             break
     else :
         tempR = A[1:]
-        # 루트보다 A[i]가 크면 1~ i-1은 왼쪽노드에, i ~ 는 오른쪽 노드
-        # A[i]가 작으면 모두 오른쪽 노드
-    
-    # 후위순회니까 왼오값 
-    post_order_traversal(tempL)
-    post_order_traversal(tempR)
-    print(root)
 
-post_order_traversal(arr)
+    post_order_to_reverse(tempL)
+    post_order_to_reverse(tempR)
+    print(root)
+        
+
+post_order_to_reverse(arr)
