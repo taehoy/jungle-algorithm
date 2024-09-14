e81d671416f1a80419099a807771c83110b28248
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-14 15:55:44 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 464 ms, Memory: 38308 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -9,23 +9,34 @@ count = 0
 graph = [[False] * (N+1) for _ in range(N+1)]
 visited = [False] * (N+1)
 
+q = []
+
 for i in range(M):
     a, b = map(int, input().split())
+
+    # # bfs에서 처음 시작 큐 설정 
+    # if i == 0 :
+    #     q.append(a)
+
     graph[a][b] = True
     graph[b][a] = True
 
-def dfs(idx):
-    global visited
+def bfs():
+    global q, visited
 
-    visited[idx] = True
+    while q :
+        cur = q.pop()
+        visited[cur] = True
 
-    for next in range(1, N+1):
-        if not visited[next] and graph[idx][next] :
-            dfs(next)
+        for next in range(1, N+1):
+            if not visited[next] and graph[cur][next] :
+                visited[next] = True
+                q.append(next)
 
 for i in range(1, N+1):
-    if visited[i] == False:
-        count +=1
-        dfs(i)
+    if not visited[i] :
+        count += 1
+        q.append(i)
+        bfs()
 
 print(count)
