d88effea1dd3dbef131f078cbae5bb110fcc5985
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-18 21:51:35 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2360 ms, Memory: 424768 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -5,32 +5,33 @@ sys.setrecursionlimit(10**9)
 arr = []
 
 while True :
-    try :
+    try:
         arr.append(int(input()))
     except :
         break
 
-def post_order_to_reverse(A):
-    # 길이가 0인 경우 리턴
-    if len(A) == 0 :
+def post_order_to_reverse(arr):
+    # 배열의 길이가0이면 return
+    if len(arr) == 0 :
         return
-    # 왼쪽 오른쪽 리스트 생성
-    tempL, tempR = [], []
-    # 루트값 정의
-    root = A[0]
+    # 왼쪽트리, 오른쪽 트리 리스트 생성
+    left, right =[], []
+    # 루트값 설정
+    root = arr[0]
 
-    # 루트값과 비교하여 큰경우 자르기 시전
-    for i in range(1, len(A)):
-        if A[i] > root :
-            tempL = A[1:i]
-            tempR = A[i:]
+    # 리스트를 돌면서 루트값보다 클경우 그 전까지의 값을 왼쪽트리에, 그 이후 값을 오른쪽 트리에 넣고 재귀함수 호출
+    for i in range(1, len(arr)):
+        if arr[i] > root :
+            left = arr[1:i]
+            right = arr[i:]
             break
+    # 루트값보다 큰게 없다면 왼쪽 노드
     else :
-        tempR = A[1:]
+        left = arr[1:]
 
-    post_order_to_reverse(tempL)
-    post_order_to_reverse(tempR)
+    # 후위순회니까 왼오값
+    post_order_to_reverse(left)
+    post_order_to_reverse(right)
     print(root)
-        
 
 post_order_to_reverse(arr)
