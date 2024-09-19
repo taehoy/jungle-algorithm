a82f5bc0faef19ac744adde9a8d2fc15231bbd81
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-19 17:28:01 +0900

    [Gold III] Title: 줄 세우기, Time: 176 ms, Memory: 38408 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -2,36 +2,35 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-v, e = map(int, input().split())
+v,e = map(int, input().split())
 
 graph = [[] for _ in range(v+1)]
 indegree = [0] * (v+1)
 
-# 초기화
 for _ in range(e):
-    a, b = map(int, input().split())
+    a, b = map(int,input().split())
     graph[a].append(b)
     indegree[b] += 1
 
-def topology_sort():
+def topology_sort() :
+    result = []
     q = deque()
-    result =[]
 
     for i in range(1, v+1):
         if indegree[i] == 0 :
             q.append(i)
-
-    while q :
+    
+    while q:
         cur = q.popleft()
 
         result.append(cur)
-        
-        for next in graph[cur] :
+
+        for next in graph[cur] : 
             indegree[next] -= 1
             if indegree[next] == 0 :
                 q.append(next)
-        
-    for i in result :
-        print(i, end= " ")
+    
+    for x in result :
+        print(x, end=' ')
 
 topology_sort()
