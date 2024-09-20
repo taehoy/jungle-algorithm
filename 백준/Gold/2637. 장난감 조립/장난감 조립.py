33920a3fc9addbe8483b535719d31a4005fd1e11
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-20 19:57:55 +0900

    [Gold II] Title: 장난감 조립, Time: 48 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -1,40 +1,43 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+from collections import deque
+sys.setrecursionlimit(10**9)
 
 n = int(input())
+m = int(input())
+
 graph = [[] for _ in range(n+1)]
 needs = [[0] * (n+1) for _ in range(n+1)]
 
 indegree = [0] * (n+1)
 
-for _ in range(int(input())) :
+for _ in range(m):
     a, b, c = map(int, input().split())
     graph[b].append((a,c))
     indegree[a] += 1
 
 def topology_sort():
     q = deque()
-    
+
     for i in range(1, n+1):
         if indegree[i] == 0 :
             q.append(i)
     
     while q :
         now = q.popleft()
-
+        
         for next, next_need in graph[now] :
-            # 기본 부품일 경우
+            # 기본 부품인 경우
             if needs[now].count(0) == n+1 :
                 needs[next][now] += next_need
-            # 중간 부품일 경우
             else :
                 for i in range(1, n+1):
                     needs[next][i] += needs[now][i] * next_need
-        
             indegree[next] -= 1
-            if indegree[next] == 0 :
+            if indegree[next]==0 :
                 q.append(next)
+            # 기본 부품이 아닌 경우
+
 
 topology_sort()
 
