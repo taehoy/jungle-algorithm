970f2ab8414cd25532ff1863d070f125262ee340
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-21 19:25:20 +0900

    [Gold III] Title: 아침 산책, Time: 56 ms, Memory: 34112 KB, Score: 60 point -BaekjoonHub

---
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -0,0 +1,61 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+from collections import deque
+
+n = int(input())
+
+s = input().rstrip()
+inout = [-1]
+for i in range(len(s)):
+    inout.append(int(s[i]))
+
+graph = [[] for _ in range(n+1)]
+
+while True :
+    try :
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    except :
+        break
+
+count = 0
+
+def dfs(first):
+    global count 
+
+    # 처음이 실외면 패스
+    if inout[first] == 0 :
+        return
+    
+    stack = [first]
+    visited[first] = True
+
+    while stack :
+        now = stack.pop()
+
+        for next in graph[now]:
+            if not visited[next]:
+                # 실내 -> 실내 : 종료
+                if inout[now] == 1 and inout[next] == 1 :
+                    count +=1
+                    continue
+                # 실내 -> 실외
+                elif inout[now] == 1 and inout[next] == 0 :
+                    stack.append(next)
+                    visited[next] = True
+                # 실외 -> 실내 : 다음 장소가 끝장소
+                elif inout[now] == 0 and inout[next] == 1:
+                    count +=1
+                    continue
+                # 실외 -> 실외
+                elif inout[now] == 0 and inout[next] == 0 :
+                    stack.append(next)
+                    visited[next] = True
+
+for i in range(1, n+1):
+    visited = [False] * (n+1)
+    dfs(i)
+
+print(count)
