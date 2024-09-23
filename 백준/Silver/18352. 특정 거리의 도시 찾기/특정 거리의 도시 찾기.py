01c140e40b8053d32f60488f25068d1531b7c82f
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-23 17:02:03 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2568 ms, Memory: 172076 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,45 +1,41 @@
 import sys
 import heapq
 input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
-from collections import deque
-INF = int(1e9) # 무한 의미로 10억 설정
+sys.setrecursionlimit(10**9)
+INF = float('inf') # 무한 의미로 10억 설정
 
-n, m, k, x = map(int,input().split())
+N, M, K, X = map(int, input().split())
 
-graph = [[] for i in range(n+1)]
+graph = [[] for _ in range(N+1)]
 
-distance = [INF] * (n+1)
-
-for _ in range(m):
-    a, b = map(int,input().split())
+for _ in range(M):
+    a,b = map(int, input().split())
     graph[a].append((b,1))
 
-def dijkstra(start) :
+distances = [INF] * (N+1)
+
+def di(start):
     q = []
-
-    # 힙 저장 방식 (거리, 노드)
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:
+    heapq.heappush(q, (0,start))
+    distances[start] = 0
+    
+    while q : 
         dist, now = heapq.heappop(q)
-
-        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
-        if distance[now] < dist :
+        
+        if distances[now] < dist :
             continue
         
         for next, next_cost in graph[now]:
-            cost = dist+next_cost
+            total_cost = dist + next_cost
 
-            if cost < distance[next]:
-                distance[next] = cost
-                heapq.heappush(q, (cost, next))
-
-
-dijkstra(x)
+            if total_cost < distances[next] :
+                distances[next] = total_cost
+                heapq.heappush(q, (total_cost, next))
+    
+di(X)
 flag = False
-for i in range(1, n+1):
-    if distance[i] == k :
+for i in range(1, N+1):
+    if distances[i] == K :
         flag = True
         print(i)
 
