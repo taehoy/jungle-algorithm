3650ed26175306d90f0b1fe7daacc2118b3fe725
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-24 11:24:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 260 ms, Memory: 57372 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -13,27 +13,27 @@ for _ in range(m):
     a,b,cost = map(int,input().split())
     graph[a].append((b,cost))
 
-distance_list = [max_value] * (n+1)
+distance = [max_value] * (n+1)
 
 start_point, end_point = map(int, input().split())
 
 def di(start) :
     q = []
-    heapq.heappush(q, (0, start))
-    distance_list[start] = 0
+    heapq.heappush(q, (0,start))
+    distance[start] = 0
 
     while q :
-        dist, now = heapq.heappop(q)
+        dist, current = heapq.heappop(q)
 
-        if distance_list[now] < dist : 
+        if distance[current] < dist :
             continue
-
-        for next, next_cost in graph[now]:
+        
+        for next, next_cost in graph[current] :
             total_cost = dist + next_cost
-            if total_cost < distance_list[next] :
-                distance_list[next] = total_cost
+            if total_cost < distance[next] :
+                distance[next] = total_cost
                 heapq.heappush(q, (total_cost, next))
-    
+
 di(start_point)
-    
-print(distance_list[end_point])
+
+print(distance[end_point])
