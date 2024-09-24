835620aa447c314f14617be97e89a66567099ae6
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-24 16:48:45 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 34228 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+from collections import deque
+
+n = int(input())
+
+nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+min_value = sys.maxsize
+max_value = -sys.maxsize
+
+def dfs(arr, result, idx):
+    global n, min_value, max_value
+
+    # 계산한 횟수가 n번이면 값 비교하고 리턴시킴
+    if n == idx :
+        min_value = min(min_value, result)
+        max_value = max(max_value, result)
+        return
+
+    if arr[0]!= 0 :
+        arr[0] -= 1
+        dfs(arr, result + nums[idx], idx+1)
+        arr[0] +=1
+    if arr[1] != 0 :
+        arr[1] -= 1
+        dfs(arr, result - nums[idx], idx+1)
+        arr[1] += 1
+    if arr[2] != 0 :
+        arr[2] -= 1
+        dfs(arr, result * nums[idx], idx+1)
+        arr[2] += 1
+    if arr[3] != 0 :
+        arr[3] -= 1
+        if result < 0 :
+            dfs(arr, -(-result // nums[idx]), idx+1)
+        else :
+            dfs(arr, result // nums[idx], idx+1)
+        arr[3] += 1
+
+dfs(arr, nums[0], 1)
+print(max_value)
+print(min_value)
