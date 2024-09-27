7f8fb9ca2478465ef427bc24030584f8c73b9ab4
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-27 17:04:58 +0900

    [Gold V] Title: 동전 2, Time: 332 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,23 +1,29 @@
 import sys
 input = sys.stdin.readline
+INF = sys.maxsize
 
 n, k = map(int, input().split())
 
-sset = set()
-for _ in range(n):
-    sset.add(int(input()))
-
-INF = k+1
 dp = [INF] * (k+1)
 dp[0] = 0
 
-for coin in sset :
-    for j in range(1, k+1):
-        if 0 <= j-coin :
-            dp[j] = min(dp[j], dp[j-coin]+1)
+coins = set()
+
+for _ in range(n):
+    coins.add(int(input()))
+
+
+for coin in coins :
+    for i in range(1, k+1):
+        if 0 <= i-coin :
+            dp[i] = min(dp[i], dp[i-coin]+1)
 
 ans = dp[k]
+#최소 비용 출력
 
 if dp[k] == INF :
     ans = -1
+
 print(ans)
+
+#     # 나누어 떨어질때
