c2e6aae82c5fc1360eca8754821a3d45081b6cbd
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-29 17:14:32 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -10,12 +10,15 @@ for _ in range(n):
 
 result = 0
 for i in range(n-1, -1, -1):
-    coin = coins[i]
-    while True :
-        if coin > k :
-            break
+    if k == 0 :
+        break
+    #몫과 나눗셈 이용
+    #타겟 금액보다 해당 동잔이 크면 pass
+    if k < coins[i] :
+        continue
+    #타겟 금액보다 해당 동전이 작고, 나눌 수 있으면 몫 : 동전 개수 , 나머지 : 남은 타겟 금액
 
-        k -= coin
-        result += 1
+    result += k // coins[i]
+    k = k % coins[i]
 
 print(result)
