6c440d364798c120bdec8e41cbdec8f1417688ca
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-30 16:04:25 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -5,17 +5,15 @@ n, m = map(int, input().split())
 
 arr = []
 
-def dfs():
-    if len(arr) > 1 and arr[-2] > arr[-1] :
-        return
+def dfs(start):
 
     if len(arr) == m :
         print(*arr)
         return
     
-    for i in range(1, n+1):
+    for i in range(start, n+1):
         arr.append(i)
-        dfs()
+        dfs(i)
         arr.pop()
 
-dfs()
+dfs(1)
