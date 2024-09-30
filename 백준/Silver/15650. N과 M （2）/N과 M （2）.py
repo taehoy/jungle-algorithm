3f817da82ded513434c71a78c4dea077800b2d36
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-09-30 15:29:22 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = []
+
+def dfs():
+    if len(arr) > 1 and arr[-2] > arr[-1] :
+        return
+
+    if len(arr) == m :
+        print(*arr)
+        return
+    
+    for i in range(1, n+1):
+        if not i in arr :
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+dfs()
