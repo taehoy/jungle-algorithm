2c9a9dd356fcf1535f38acbedbb9605116955593
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-10-22 13:50:23 +0900

    [Gold V] Title: 적록색약, Time: 56 ms, Memory: 31644 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,40 +1,35 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 8)
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-def dfs(a, b) :
-
-    visited[a][b] = True
-    current_color = graph[a][b]
+def dfs(x, y):
+    visited[x][y] = True
+    c_color = graph[x][y]
 
     for next in range(4):
-        ny = a + dy[next]
-        nx = b + dx[next]
-        
-        if 0<= nx < n and 0<= ny < n and visited[ny][nx] == False :
-            if graph[ny][nx] == current_color :
-                dfs(ny,nx)
+        nx = x + dx[next]
+        ny = y + dy[next]
+        if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False :
+            if graph[nx][ny] == c_color:    
+                dfs(nx,ny)
 
-n = int(input().rstrip())
+n = int(input())
 
 graph = [list(input().rstrip()) for _ in range(n)]
 
 visited = [[False] * n for _ in range(n)]
 
-# 적록색약 x
-result1, result2 = 0,0
+result1, result2 = 0, 0
 
 for i in range(n):
     for j in range(n):
-        if not visited[i][j] :
+        if visited[i][j] == False :
             dfs(i,j)
-            result1+=1
+            result1 += 1
 
-# 적록색약 o 
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 'R':
@@ -44,8 +39,9 @@ visited = [[False] * n for _ in range(n)]
 
 for i in range(n):
     for j in range(n):
-        if not visited[i][j] :
+        if visited[i][j] == False :
             dfs(i,j)
             result2 += 1
 
 print(result1, result2)
+
