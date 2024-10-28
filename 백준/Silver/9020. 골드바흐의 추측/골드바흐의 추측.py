66db1b5ab65974bff5c75714d27849500c6304e7
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-10-28 09:06:22 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 188 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,11 +1,13 @@
-def isPrime(num):
-    if num ==1:
-        return False
+import sys
+input = sys.stdin.readline
 
-    for i in range(2, int(num**0.5)+1):
-        if num % i == 0:
-            return False 
+def isPrime(num):
+    if num == 1 :
+        return False
     
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0 :
+            return False
     return True
 
 T = int(input())
@@ -13,15 +15,12 @@ T = int(input())
 for _ in range(T) :
     num = int(input())
 
-    a,b = num//2, num//2
+    a, b = num//2, num//2
+
     while a>0 :
-        if isPrime(a) and isPrime(b):
+        if isPrime(a) and isPrime(b) :
             print(a, b)
             break
         else:
             a -= 1
             b += 1
-
-
-
-
