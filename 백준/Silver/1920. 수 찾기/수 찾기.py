7ad3383975f10d4a1e9ce99da3e8024b40981646
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-11-14 09:34:32 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 50520 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,22 +1,11 @@
-N = int(input())
-a = list(map(int, input().split()))
-a.sort()
+import sys
+input = sys.stdin.readline
 
-M = int(input())
-b = list(map(int, input().split()))
+n = int(input())
+arr = set(map(int, input().split()))
 
-def binarySearch(target):
-    st = 0
-    en = len(a)-1
-    while(st <= en):
-        mid = (st+en) // 2
-        if a[mid] > target :
-            en = mid-1
-        elif a[mid] < target :
-            st = mid+1
-        else :
-            return 1    
-    return 0
+m = int(input())
+nums = list(map(int, input().split()))
 
-for i in b:
-    print(binarySearch(i))
+for num in nums :
+    print(1) if num in arr else print(0)
