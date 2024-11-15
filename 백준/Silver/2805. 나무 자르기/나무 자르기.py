8cbd6a24e5c29386fb40c241557991555d3f5d6a
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-11-15 14:38:23 +0900

    [Silver II] Title: 나무 자르기, Time: 436 ms, Memory: 258028 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,30 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-# N: 나무의 수, target: 가져가려는 나무의 길이
-N, target = map(int, input().split())
+n, target = map(int, input().split())
+
 trees = list(map(int, input().split()))
 
-# 나무 자르기 함수: mid 높이로 자를 때 얻을 수 있는 나무 길이
-def sliceTree(mid):
-    cur = 0
-    for tree in trees:
-        if tree > mid:
-            cur += tree - mid
-    return cur
+start = 1
+end = max(trees)
 
-# 이진 탐색 시작
-start, end = 0, max(trees)
-result = 0
-
-while start <= end:
+while start <= end :
     mid = (start + end) // 2
-    count = sliceTree(mid)
-    
-    if count >= target:  # 목표 나무 길이보다 크거나 같을 때
-        result = mid  # 일단 가능한 높이로 저장
-        start = mid + 1  # 더 높은 높이로 시도
-    else:
-        end = mid - 1  # 나무 길이가 부족하면 낮은 높이로 시도
 
-print(result)
+    total = 0 # 잘린 나무의 길이 총합
+    for i in trees:
+        if i >= mid :
+            total += i - mid
+
+    if total >= target :
+        start = mid + 1
+    else :
+        end = mid - 1
+
+print(end)
