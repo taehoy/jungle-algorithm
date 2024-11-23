18fdebe08d25641f419c6eabac2b54d60018f8b0
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-11-23 22:31:59 +0900

    [Gold IV] Title: 불, Time: 2444 ms, Memory: 77744 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-dx = [1, 0, -1, 0]
+dx= [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-def burn() :
+def burn():
     while queue :
         x, y, t = queue.popleft()
 
@@ -13,7 +13,7 @@ def burn() :
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < h and 0 <= ny < w :
+            if 0<= nx < h and 0<= ny < w :
                 if graph[nx][ny] != '#' and visited[nx][ny] == 0 :
                     visited[nx][ny] = t + 1
                     queue.append([nx, ny, t+1])
@@ -24,38 +24,39 @@ def bfs(x, y) :
     q = deque()
     q.append([x, y, 1])
 
-    while q : 
-        cx, cy, t = q. popleft()
+    while q :
+        cx, cy, t = q.popleft()
 
-        if cx == 0 or cx == h -1 or cy == 0 or cy == w - 1 :
+        if cx == 0 or cx == h - 1 or cy == 0 or cy == w - 1:
             return t
         
-        for i in range(4):
+        for i in range(4) :
             nx = cx + dx[i]
             ny = cy + dy[i]
 
-            if 0 <= nx < h and 0 <= ny < w :
+            if 0<=nx < h and 0 <= ny < w :
                 if graph[nx][ny] != '#' :
                     if visit[nx][ny] > t + 1 and visit[nx][ny] != -1 or visited[nx][ny] == 0 :
-                        q.append([nx,ny, t+1])
+                        q.append([nx, ny, t + 1])
                         visit[nx][ny] = -1
 
-    return 'IMPOSSIBLE'
+    return "IMPOSSIBLE"
 
 for _ in range(int(input())):
-    w, h = map (int, input().split())
+    w, h = map(int, input().split())
     graph = [list(map(str, input().rstrip())) for _ in range(h)]
 
     # 불이 붙은 위치 담을 큐
     queue = deque()
     visited = [[0] * w for _ in range(h)]
 
+    
     for i in range(h):
-        for j in range(w) :
-            # 해당 구역에 불이 붙으면 큐에 좌표 삽입
+        for j in range(w):
+            # 해당 구역에 불이 부트면 큐에 좌표 삽입
             if graph[i][j] == '*' :
-                queue.append([i, j, 1])
-                visited[i][j] = 1
+                queue.append([i,j,1]) # 3번째 값은 불이 붙은 날짜. 처음은 1일차
+                visited[i][j] = 1 # 1이면 불이 붙은거임!
             elif graph[i][j] == '@':
                 sx, sy = i, j
     
